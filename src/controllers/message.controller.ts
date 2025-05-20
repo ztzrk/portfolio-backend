98b8fa8aa09efabd3792644b1123687e2429b086
ztzrk <ralif877@gmail.com>
2025-05-20 13:00:21 +0800

    refactor: update getMessages to return a static message instead of fetching from the database

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -17,8 +17,7 @@ export const submitMessage = async (req: Request, res: Response) => {
 
 export const getMessages = async (req: Request, res: Response) => {
     try {
-        const messages = await Message.find();
-        res.status(200).json(messages);
+        res.status(200).json({ message: "dapat mi" });
     } catch (error) {
         res.status(500).json({ message: "Error fetching messages", error });
     }
