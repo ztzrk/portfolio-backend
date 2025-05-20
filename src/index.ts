616ba9f9661a12207e482d80a2bf31b0d3cc2e9b
ztzrk <ralif877@gmail.com>
2025-05-20 16:16:58 +0800

    fix: update CORS origin to allow all origins

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
     cors({
-        origin: ["portfolio-aflah-alifus-projects.vercel.app"],
+        origin: ["*"],
         methods: ["GET", "POST", "OPTIONS"],
         allowedHeaders: ["Content-Type"],
     })
